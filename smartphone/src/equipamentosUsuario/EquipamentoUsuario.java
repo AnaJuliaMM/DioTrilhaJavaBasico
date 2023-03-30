50a82862c3d37576b5a234713a810dae7cfb1fe6
Ana Julia Moura Martins <ana.martins_2004@outlook.com>
2023-03-30 14:52:48 -0300

    Mudança de iphone para smartphone

--- smartphone/src/equipamentosUsuario/EquipamentoUsuario.java
+++ smartphone/src/equipamentosUsuario/EquipamentoUsuario.java
@@ -1,13 +1,16 @@
-package IphoneUsuario;
+package equipamentosUsuario;
 
 import equipamento.Iphone;
+import equipamento.Samsung;
 import interfaces.AparelhoTelefonico;
 import interfaces.NavegadorInternet;
 import interfaces.ReprodutorMusicial;
 
-public class IphoneUsuario {
+public class EquipamentoUsuario {
 
 	public static void main(String[] args) {
+		
+		System.out.println("1. Equipamento: Iphone\n");
 		
 		Iphone iphone = new Iphone(); 
 		
@@ -28,10 +31,27 @@ public class IphoneUsuario {
 		navegadorInternet.atualizarPagina();
 		navegadorInternet.exibirPagina();
 		
+		System.out.println("\n2. Equipamento: Samsung\n");
 		
-			
+		Samsung samsung = new Samsung();
+		
+		reprodutorMusical = samsung;
+		aparelhoTelefonico = samsung;
+		navegadorInternet = samsung;
+		
+		reprodutorMusical.selecionarMusica();  
+		reprodutorMusical.tocar();
+		reprodutorMusical.pausar();
+		
+		aparelhoTelefonico.atender();
+		aparelhoTelefonico.ligar();
+		aparelhoTelefonico.iniciarCorreioVoz();
+		
+		navegadorInternet.adicionarNovaAba();
+		navegadorInternet.atualizarPagina();
+		navegadorInternet.exibirPagina();
 		
 		
-
 	}
+
 }
